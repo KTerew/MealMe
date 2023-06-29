c5cf3c4f9a017b7a038f22085f4d6c72cc787b97
KTerew <Terewkody@gmail.com>
2023-06-29 18:14:47 -0400

    Initial Commit

---
+++ MealMe.Data/MealMeContext/MealMeDBContext.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MealMe.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MealMe.Data.MealMeContext
+{
+    public class MealMeDBContext : DbContext
+    {
+        public MealMeDBContext(DbContextOptions options) : base(options) {}
+
+        public DbSet<Meal> Meals { get; set; }
+        public DbSet<Ingredient> Ingredients { get; set; }
+        public DbSet<Cuisine> Cuisines { get; set; }
+    }
+}
