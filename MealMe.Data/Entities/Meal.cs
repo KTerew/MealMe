138cbaf732f555e8f92a54a57553fd90de03f7a9
KTerew <Terewkody@gmail.com>
2023-06-29 18:47:51 -0400

    Added Meal Entity and Meal Models

--- MealMe.Data/Entities/Meal.cs
+++ MealMe.Data/Entities/Meal.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +9,44 @@ namespace MealMe.Data.Entities
 {
     public class Meal
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Description { get; set; }
+
+        [Required]
+        public int Calories { get; set; }
+
+        [Required]
+        public int PrepTime { get; set; }
+
+        [ForeignKey("Cuisine")]
+        public int CuisineId { get; set; }
         
+        public virtual Cuisine Cuisine { get; set; }
+        
+        public virtual List<Ingredient> Ingredients { get; set; } = new List<Ingredient>();
+
+        // public double Price
+        // {
+        //     get
+        //     {
+        //         if(Ingredients.Count == 0)
+        //             return 0;
+
+        //         double total = 0.0;
+        //         foreach (Ingredient ingredient in Ingredients)
+        //         {
+        //             total += ingredient.Price;
+        //         }
+        //         return total;
+        //     }
+        // }
     }
 }
--- MealMe.Models/Models/MealModels/MealListItem.cs
+++ MealMe.Models/Models/MealModels/MealListItem.cs
@@ -2,11 +2,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MealMe.Data.Entities;
 
 namespace MealMe.Models.Models.MealModels
 {
     public class MealListItem
     {
-        
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int Calories { get; set; }
+
+        public int PrepTime { get; set; }
     }
 }
