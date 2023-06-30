02e276ccdcace5b9fd7f52b82a70d663ad53d07a
KTerew <Terewkody@gmail.com>
2023-06-29 20:41:45 -0400

    Added MealServices and MealControllers

--- MealMe.Services/Services/MealServices/IMealServices.cs
+++ MealMe.Services/Services/MealServices/IMealServices.cs
@@ -2,11 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MealMe.Models.Models.MealModels;
 
 namespace MealMe.Services.Services.MealServices
 {
     public interface IMealServices
     {
-        
+        Task<bool> CreateMeal(MealCreate model);
+        Task<bool> UpdateMeal(MealEdit model);
+        Task<bool> DeleteMeal(int id);
+        Task<MealDetail> GetMeal(int id);
+        Task<List<MealListItem>> GetMeals();
     }
 }
--- MealMe.Services/Services/MealServices/MealServices.cs
+++ MealMe.Services/Services/MealServices/MealServices.cs
@@ -2,11 +2,69 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using MealMe.Data.Entities;
+using MealMe.Data.MealMeContext;
+using MealMe.Models.Models.MealModels;
+using Microsoft.EntityFrameworkCore;
 
 namespace MealMe.Services.Services.MealServices
 {
     public class MealServices : IMealServices
     {
-        
+        private readonly MealMeDBContext _context;
+        private IMapper _mapper;
+
+        public MealServices(MealMeDBContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<bool> CreateMeal(MealCreate model)
+        {
+            var meal = _mapper.Map<Meal>(model);
+
+            await _context.Meals.AddAsync(meal);
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> DeleteMeal(int id)
+        {
+            var meal = await _context.Meals.FindAsync(id);
+            if(meal is null)
+                return false;
+
+            _context.Meals.Remove(meal);
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<MealDetail> GetMeal(int id)
+        {
+            var meal = await _context.Meals.Include(m => m.Ingredients).FirstOrDefaultAsync(x=>x.Id == id);
+            if(meal is null)
+                return new MealDetail();
+            
+            var MealDetail = _mapper.Map<MealDetail>(meal);
+
+            return MealDetail;
+        }
+
+        public async Task<List<MealListItem>> GetMeals()
+        {
+            var meals = await _context.Meals.ToListAsync();
+            var mealListItem = _mapper.Map<List<MealListItem>>(meals);
+
+            return mealListItem;
+        }
+
+        public async Task<bool> UpdateMeal(MealEdit model)
+        {
+            var meal = await _context.Meals.FirstOrDefaultAsync(x=>x.Id == model.Id);
+
+            var conversion = _mapper.Map<MealEdit,Meal>(model, meal);
+            _context.Meals.Update(conversion);
+            return await _context.SaveChangesAsync() > 0;
+        }
     }
 }
