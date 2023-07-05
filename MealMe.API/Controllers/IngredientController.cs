e598209a29f8fed90e4f412e895cd377fb831c2a
Kristen Swaim <106117219+kdswaim@users.noreply.github.com>
2023-07-05 18:09:05 -0400

    added ingredient controller

--- MealMe.API/Controllers/IngredientController.cs
+++ MealMe.API/Controllers/IngredientController.cs
@@ -10,6 +10,46 @@ namespace MealMe.API.Controllers
     [Route("api/[controller]")]
     public class IngredientController : ControllerBase
     {
-        
+         private readonly IIngredientServices _ingredientServices;
+
+        public IngredientController(IIngredientServices ingredientServices)
+        {
+            _ingredientServices = ingredientServices;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateIngredient(IngredientCreate model)
+        {
+            bool createIngredient = await _ingredientServices.CreateMeal(model);
+            return Ok(createIngredient);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteIngredient(int id)
+        {
+            bool deleteIngredient = await _ingredientServices.DeleteIngredient(id);
+            return Ok(deleteIngredient);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetIngredients()
+        {
+            List<IngredientListItem> getIngredients = await _ingredientServices.GetIngredients();
+            return Ok(getIngredients);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetIngredients(int id)
+        {
+            IngredientDetail getIngredient = await _ingredientServices.GetIngredient(id);
+            return Ok(getIngredient);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateIngredients(IngredientEdit model)
+        {
+            bool updateIngredient = await _ingredientServices.UpdateIngredient(model);
+            return Ok(updateIngredient);
+        }
     }
 }
