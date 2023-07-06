053f222e0d3c9108f624129899b8b472bcf0d61e
Kristen Swaim <106117219+kdswaim@users.noreply.github.com>
2023-07-06 18:31:35 -0400

    Update IngredientController.cs

--- MealMe.API/Controllers/IngredientController.cs
+++ MealMe.API/Controllers/IngredientController.cs
@@ -1,57 +1,55 @@
-// using System;
-// using System.Collections.Generic;
-// using System.Linq;
-// using System.Threading.Tasks;
-// using MealMe.Models.Models.IngredientModels;
-// using MealMe.Services.Services.IngredientServices;
-// using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
-// namespace MealMe.API.Controllers
-// {
-//     [ApiController]
-//     [Route("api/[controller]")]
-//     public class IngredientController : ControllerBase
-//     {
-//          private readonly IIngredientServices _ingredientServices;
+namespace MealMe.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class IngredientController : ControllerBase
+    {
+         private readonly IIngredientServices _ingredientServices;
 
-//         public IngredientController(IIngredientServices ingredientServices)
-//         {
-//             _ingredientServices = ingredientServices;
-//         }
+        public IngredientController(IIngredientServices ingredientServices)
+        {
+            _ingredientServices = ingredientServices;
+        }
 
-//         [HttpPost]
-//         public async Task<IActionResult> CreateIngredient(IngredientCreate model)
-//         {
-//             bool createIngredient = await _ingredientServices.CreateMeal(model);
-//             return Ok(createIngredient);
-//         }
+        [HttpPost]
+        public async Task<IActionResult> CreateIngredient(IngredientCreate model)
+        {
+            bool createIngredient = await _ingredientServices.CreateMeal(model);
+            return Ok(createIngredient);
+        }
 
-//         [HttpDelete]
-//         public async Task<IActionResult> DeleteIngredient(int id)
-//         {
-//             bool deleteIngredient = await _ingredientServices.DeleteIngredient(id);
-//             return Ok(deleteIngredient);
-//         }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteIngredient(int id)
+        {
+            bool deleteIngredient = await _ingredientServices.DeleteIngredient(id);
+            return Ok(deleteIngredient);
+        }
 
-//         [HttpGet]
-//         public async Task<IActionResult> GetIngredients()
-//         {
-//             List<IngredientListItem> getIngredients = await _ingredientServices.GetIngredients();
-//             return Ok(getIngredients);
-//         }
+        [HttpGet]
+        public async Task<IActionResult> GetIngredients()
+        {
+            List<IngredientListItem> getIngredients = await _ingredientServices.GetIngredients();
+            return Ok(getIngredients);
+        }
 
-//         [HttpGet]
-//         public async Task<IActionResult> GetIngredients(int id)
-//         {
-//             IngredientDetail getIngredient = await _ingredientServices.GetIngredient(id);
-//             return Ok(getIngredient);
-//         }
+        [HttpGet]
+        public async Task<IActionResult> GetIngredients(int id)
+        {
+            IngredientDetail getIngredient = await _ingredientServices.GetIngredient(id);
+            return Ok(getIngredient);
+        }
 
-//         [HttpPut]
-//         public async Task<IActionResult> UpdateIngredients(IngredientEdit model)
-//         {
-//             bool updateIngredient = await _ingredientServices.UpdateIngredient(model);
-//             return Ok(updateIngredient);
-//         }
-//     }
-// }
+        [HttpPut]
+        public async Task<IActionResult> UpdateIngredients(IngredientEdit model)
+        {
+            bool updateIngredient = await _ingredientServices.UpdateIngredient(model);
+            return Ok(updateIngredient);
+        }
+    }
+}
